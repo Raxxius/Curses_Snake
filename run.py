9cde4d3ee63ac8d2cf2f3bdc7073de0fbde42174
Raxxius <Raxxman@gmail.com>
2021-12-16 16:26:11 +0000

    updated imports

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import curses
 import time
 import gspread
 from curses import wrapper, KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
-from curses.textpad import Textbox
+from curses.textpad import Textbox, rectangle
 from google.oauth2.service_account import Credentials
 
 
@@ -17,12 +17,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('curses_snake')
 
+def main(stdscr):
 
+    stdscr.clear()
 # Game area
-curses.newwin(23, 79, 0, 0)
+    gamearea = curses.newwin(23, 79, 0, 0)
+    gamearea.clear()
 
-try:
-    rectangle(stdscr, 0, 0, 23, 79)
-except curses.error:
-    pass
+    try:
+        rectangle(stdscr, 0, 0, 23, 79)
+    except curses.error:
+        pass
 
+
+wrapper(main)
